c02273b436bbc7ef172df4d41370b54553b3df52
Jhen Nuñez <jxelektro@gmail.com>
2022-11-16 15:07:32 -0300

    CSS

--- src/routes/dashboardView.jsx
+++ src/routes/dashboardView.jsx
@@ -1,7 +1,23 @@
-
+import Trial from "../assets/Trial.jpg";
 
 export default function () {
   return (
-    <div>Dashboard</div>
+    <div className="dashboardContainer">
+      <div className="dashboardSider">
+        <h1>LarnU</h1>
+        <h2>Dashboard</h2>
+        <h2>Push</h2>
+      </div>
+      <div className="dashboardView">
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+        <img className="Trial" src={Trial} alt="trial" />
+      </div>
+    </div>
   );
 }
