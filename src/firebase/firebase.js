c020596d063e042d83b7d1d1d0a0e10072cac4d9
Peter Neville <neville.peter.2000@gmail.com>
2022-11-17 15:41:10 -0500

    despliegue en firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getStorage, ref, uploadBytes, getDownloadURL, getBytes } from 'firebase/storage';
 import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, setDoc, deleteDoc } from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
-  measurementId: process.env.REACT_APP_MEASUREMENTID
+  apiKey: process.env.REACT_APP_APIKEY || "AIzaSyCP0CIbfvR48T9i-gMkB41rj_Gw8x18GqU",
+  authDomain: process.env.REACT_APP_AUTHDOMAIN || "usera-77c6e.firebaseapp.com",
+  projectId: process.env.REACT_APP_PROJECTID || "usera-77c6e",
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET || "usera-77c6e.appspot.com",
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID || "931714051539",
+  appId: process.env.REACT_APP_APPID || "1:931714051539:web:403c485cf75b311d74d326",
+  measurementId: process.env.REACT_APP_MEASUREMENTID || "G-M64EG8E4DW",
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
 
 export async function userExist (uid) {
   const docRef = doc(db, "users", uid);
