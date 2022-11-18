d17c80d6d1e6d9b47be4463901eda07af9a520de
Jhen Nuñez <jxelektro@gmail.com>
2022-11-18 14:42:49 -0300

    Css Update

--- src/routes/dashboardView.jsx
+++ src/routes/dashboardView.jsx
@@ -1,22 +1,27 @@
-import dalletest1 from '../assets/Dalle/dalletest1.png';
-import dalletest2 from '../assets/Dalle/dalletest2.png';
-import dalletest3 from '../assets/Dalle/dalletest3.png';
-import dalletest4 from '../assets/Dalle/dalletest4.png';
-import dalletest5 from '../assets/Dalle/dalletest5.png';
-import dalletest6 from '../assets/Dalle/dalletest6.png';
-import dalletest7 from '../assets/Dalle/dalletest7.png';
-
+import dalletest1 from "../assets/Dalle/dalletest1.png";
+import dalletest2 from "../assets/Dalle/dalletest2.png";
+import dalletest3 from "../assets/Dalle/dalletest3.png";
+import dalletest4 from "../assets/Dalle/dalletest4.png";
+import dalletest5 from "../assets/Dalle/dalletest5.png";
+import dalletest6 from "../assets/Dalle/dalletest6.png";
+import dalletest7 from "../assets/Dalle/dalletest7.png";
+import logo from "../assets/logo.svg";
 
 /* this is a test to imagine the dashboard */
-
 export default function dashboard() {
   return (
     <div className="dashboardContainer">
       <div className="dashboardSider">
-        <h1>LarnU</h1>
-        <h2>Dashboard</h2> 
+        <div className="dashboardSiderLogo">
+          <img className="logo_dashboard" src={logo} alt="logo" />
+        </div>
+        <h2>Dashboard</h2>
         <h2>Push</h2>
-      </div> 
+        <div className="dashboardLogOutContainer">
+          <h2 className="dashboardLogOut">Log Out</h2>
+        </div>
+      </div>
+
       <div className="dashboardView">
         <img src={dalletest1} alt="dalletest1" />
         <img src={dalletest2} alt="dalletest2" />
