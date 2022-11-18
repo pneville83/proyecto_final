fd99134497bb8c1230f01bf53ef4ca58349a7d63
Peter Neville <neville.peter.2000@gmail.com>
2022-11-18 15:13:27 -0500

    incluyendo variables de entorno

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore, collection, addDoc, getDocs, doc, getDoc, query, where, s
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY || "AIzaSyCP0CIbfvR48T9i-gMkB41rj_Gw8x18GqU",
-  authDomain: process.env.REACT_APP_AUTHDOMAIN || "usera-77c6e.firebaseapp.com",
-  projectId: process.env.REACT_APP_PROJECTID || "usera-77c6e",
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET || "usera-77c6e.appspot.com",
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID || "931714051539",
-  appId: process.env.REACT_APP_APPID || "1:931714051539:web:403c485cf75b311d74d326",
-  measurementId: process.env.REACT_APP_MEASUREMENTID || "G-M64EG8E4DW",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
 export const app = initializeApp(firebaseConfig);
